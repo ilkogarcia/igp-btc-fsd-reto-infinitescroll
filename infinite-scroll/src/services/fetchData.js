93ad9b32e29cbb11a4903215f9cd1cad49311e65
ilko.garcia <ilko.garcia@gmail.com>
2023-03-27 22:53:49 +0200

    fix: change fetchData function to async/await

--- infinite-scroll/src/services/fetchData.js
+++ infinite-scroll/src/services/fetchData.js
@@ -1,9 +1,8 @@
 const RICKANDMORTY_API_URL = 'https://rickandmortyapi.com/api/character/'
 
-export const fetchData = (page) => {
-  const promise = fetch(`${RICKANDMORTY_API_URL}?page=${page}`)
-    .then((res) => res.json())
-  return promise
+export const fetchData = async (page) => {
+  const data = await fetch(`${RICKANDMORTY_API_URL}?page=${page}`).then((res) => res.json())
+  return data
 }
 
 export default fetchData
