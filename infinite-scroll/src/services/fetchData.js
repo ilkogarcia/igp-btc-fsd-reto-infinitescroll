fe42f61f6b81522db3687c0d43f780e15add0a9d
ilko.garcia <ilko.garcia@gmail.com>
2023-03-30 10:56:06 +0200

    refactor: we extract the logic related to queries to an API to a separate functional module

--- infinite-scroll/src/services/fetchData.js
+++ infinite-scroll/src/services/fetchData.js
@@ -1,8 +1,7 @@
 const RICKANDMORTY_API_URL = 'https://rickandmortyapi.com/api/character/'
 
 export const fetchData = async (page) => {
-  const data = await fetch(`${RICKANDMORTY_API_URL}?page=${page}`).then((res) => res.json())
-  return data
+  return fetch(`${RICKANDMORTY_API_URL}?page=${page}`).then((res) => res.json())
 }
 
 export default fetchData
